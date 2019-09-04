23f18aceee57830031f4bf34be6506e355fae46d
Phillip Malboeuf <phil@phils.computer>
2019-09-04 10:14:54 -0400

    no webp

--- src/components/Picture.svelte
+++ src/components/Picture.svelte
@@ -11,12 +11,12 @@
 
 <picture>
   {#if small}
-  <source srcSet="{media.fields.file.url}?w=400&fm=webp" media="(max-width: 900px)" />
-  <source srcSet="{media.fields.file.url}?w=600&fm=webp" media="(max-width: 1200px)" />
-  <img src="{media.fields.file.url}?w=800&fm=webp" alt="{media.fields.title} {media.fields.description}" />
+  <source srcSet="{media.fields.file.url}?w=400" media="(max-width: 900px)" />
+  <source srcSet="{media.fields.file.url}?w=600" media="(max-width: 1200px)" />
+  <img src="{media.fields.file.url}?w=800" alt="{media.fields.title} {media.fields.description}" />
   {:else}
-  <source srcSet="{media.fields.file.url}?w=900&fm=webp" media="(max-width: 900px)" />
-  <source srcSet="{media.fields.file.url}?w=1200&fm=webp" media="(max-width: 1200px)" />
-  <img src="{media.fields.file.url}?w=1800&fm=webp" alt="{media.fields.title} {media.fields.description}" />
+  <source srcSet="{media.fields.file.url}?w=900" media="(max-width: 900px)" />
+  <source srcSet="{media.fields.file.url}?w=1200" media="(max-width: 1200px)" />
+  <img src="{media.fields.file.url}?w=1800" alt="{media.fields.title} {media.fields.description}" />
   {/if}
 </picture>
